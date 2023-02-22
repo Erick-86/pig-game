6184f15f74a8034786bb624d58b0b1de994794a2
FreeMan <zelonaire@gmail.com>
2023-02-22 19:10:41 +0000

    creating variables for the score elements

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const rollDiceBtn = document.querySelector(".roll-dice");
 const holdBtn = document.querySelector('.hold-btn')
 const newGameBtn = document.querySelector('.new-game-btn')
-
+const playerScore = document.querySelectorAll('.player-score')
+const currentScore = document.querySelectorAll('.current-score')
 
